8ba19d08ba817d29c82f57810c1afb6de6e84f30
Monica Carvajal <monik.182@gmail.com>
2024-07-22 22:12:49 +0200

    - reorg files - add webpack copy - update webpack config

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -1,5 +1,5 @@
-// webpack.config.js
-const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
 
 module.exports = {
   entry: {
@@ -12,5 +12,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js'
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'manifest.json'), to: path.resolve(__dirname, 'dist') },
+        { from: path.resolve(__dirname, 'popup.html'), to: path.resolve(__dirname, 'dist') }
+      ]
+    })
+  ],
   mode: 'production'
-};
+}
