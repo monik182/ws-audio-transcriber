5b75f5469cc64bb847e58d8fca739084f1b8d36c
Monica Carvajal <monik.182@gmail.com>
2024-09-20 00:43:02 +0200

    bg experiments

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,3 +1,7 @@
+import { getFromStorage, saveToStorage, TOKEN_KEY } from './utils';
+
+const TRANSCRIPTION_QUEUE_KEY = 'transcriptionQueue';
+const SAVED_TRANSCRIPTIONS_KEY = 'savedTranscriptions';
 
 chrome.runtime.onInstalled.addListener(details => {
   console.log('installed details>>>>', details)
@@ -5,31 +9,66 @@ chrome.runtime.onInstalled.addListener(details => {
 
 chrome.tabs.onActivated.addListener(init);
 
+async function emitTranscriptionEvent(id, tabId, token, oggBlobURL) {
+  const transcription = await getTranscription(token, oggBlobURL)
+  // const transcription = { text: `${id} - MOCKED TRANSCRIPTION - ${oggBlobURL}` }
+  console.log('#***TRANSCRIPTION>>>', transcription)
+  await saveTranscription(id, {
+    text: transcription.text || transcription.error?.message || "",
+    error: transcription.text == null
+  });
+  chrome.tabs.sendMessage(tabId, {
+    id,
+    oggBlobURL,
+  });
+}
+
+let counter = 0;
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  console.log('a message in the background....', request)
-  chrome.storage.sync.get("token", async result => {
-    console.log('********TOKEN>>>', result)
-    if (!result.token) return;
-    const { token } = result;
+  // console.log('a message in the background....', request)
+  // console.log('******** OUTSIDE TOKEN>>>', token)
+
+  // chrome.storage.sync.get("token", async result => {
+  //   console.log('********TOKEN>>>', result)
+  //   if (!result.token) return;
+  //   const { token } = result;
 
     if (request.action === 'fromContent') {
-      // let oggBlob = await fetch(request.oggBlobURL);
-      // oggBlob = await oggBlob.blob();
-      // console.log('request inside oggBlob', oggBlob)
-      console.log('request inside oggBlob URL', request.oggBlobURL)
-      const transcription = await getTranscription(token, request.oggBlobURL)
-      console.log('***TRANSCRIPTION>>>', transcription)
-      saveTranscription(request.id, {
-        text: transcription.text || transcription.error?.message || "",
-        error: transcription.text == null
-      });
-      chrome.tabs.sendMessage(sender.tab.id, {
-        id: request.id,
-        oggBlobURL: request.oggBlobURL
-      });
+      counter++;
+      console.log('***********************', {counter}, '***************************')
+      // if (counter > 3) return;
+      const { id } = request;
+      // const transcriptionQueue = await getFromStorage(TRANSCRIPTION_QUEUE_KEY) || []
+      const savedMessage = await getFromStorage(id)
+      // const savedTranscriptions = await getFromStorage(SAVED_TRANSCRIPTIONS_KEY) || {}
+      // console.log('CURRENT SAVED TRANSCIPRIONS', savedTranscriptions)
+      // const message = savedTranscriptions[id]
+      console.log(id, '>>>>>>>>>CURRENT savedMessage', savedMessage)
+      if (!savedMessage) {
+        console.log(counter, 'CRETING TRANSCRIPTION FRO ID', id, savedMessage)
+        // console.log(id, 'CURRENT transcriptionQueue', transcriptionQueue)
+        const token = await getFromStorage(TOKEN_KEY);
+        await emitTranscriptionEvent(id, sender.tab.id, token, request.oggBlobURL)
+        // await saveToStorage(TRANSCRIPTION_QUEUE_KEY, [...transcriptionQueue, id])
+      }
+  //     // let oggBlob = await fetch(request.oggBlobURL);
+  //     // oggBlob = await oggBlob.blob();
+  //     // console.log('request inside oggBlob', oggBlob)
+  //     // >>>>await emitTranscriptionEvent(request.id, sender.tab.id, token, request.oggBlobURL)
+  //     // console.log('request inside oggBlob URL', request.oggBlobURL)
+  //     // const transcription = await getTranscription(token, request.oggBlobURL)
+  //     // console.log('***TRANSCRIPTION>>>', transcription)
+  //     // saveTranscription(request.id, {
+  //     //   text: transcription.text || transcription.error?.message || "",
+  //     //   error: transcription.text == null
+  //     // });
+  //     // chrome.tabs.sendMessage(sender.tab.id, {
+  //     //   id: request.id,
+  //     //   oggBlobURL: request.oggBlobURL
+  //     // });
     }
 
-  });
+  // });
 });
 
 async function getTranscription(token, blobUrl) {
@@ -64,7 +103,7 @@ async function getCurrentTab() {
   return tab;
 }
 
-function execute(tab) {
+function executeContentScript(tab) {
   // console.log('CURRENT TAB>>>>', tab)
   if (tab.url.includes("web.whatsapp.com")) {
     chrome.scripting.executeScript({
@@ -82,16 +121,71 @@ function execute(tab) {
 }
 
 async function init() {
+  // const transcriptionQueue = await getFromStorage(TRANSCRIPTION_QUEUE_KEY)
+  // if (!transcriptionQueue) {
+  //   await saveToStorage(TRANSCRIPTION_QUEUE_KEY, [])
+  // }
+  const token = await getFromStorage(TOKEN_KEY);
+
+  // chrome.storage.sync.clear() // TODO: remove this
+  await saveToStorage(TOKEN_KEY, token)
+  // chrome.storage.sync.get(null, result => {
+  //   console.log('storage local', result)
+  //   Object.keys(result).forEach(key => {
+  //     if (key === TOKEN_KEY) return;
+  //     chrome.storage.local.remove(key);
+  //   });
+  // })
+
   const tab = await getCurrentTab()
 
   if (tab) {
-    execute(tab)
+    executeContentScript(tab)
   }
 }
 
 
 
-function saveTranscription(key, value) {
+async function saveTranscriptionNOTUSE(key, value) {
+  const savedTranscriptions = await getFromStorage(SAVED_TRANSCRIPTIONS_KEY) || {}
+
+  const updatedTranscriptions = {...savedTranscriptions};
+
+  updatedTranscriptions[key] = {
+    value,
+    timestamp: Date.now()
+  };
+
+  const size = JSON.stringify(updatedTranscriptions[key]).length;
+  const quota = chrome.storage.local.QUOTA_BYTES;
+
+  console.log('SIZE>>>', size)
+  console.log('QUOTA>>>', quota)
+
+  if (size >= quota) {
+    const keys = Object.keys(updatedTranscriptions);
+    const memory = [];
+
+    keys.forEach(key => {
+      memory.push({
+        key,
+        timestamp: updatedTranscriptions[key].timestamp
+      });
+    });
+
+    memory.sort((a, b) => {
+      return a.timestamp - b.timestamp;
+    });
+
+    for (let i = 0; i < memory.length / 2; i++) {
+      delete updatedTranscriptions[memory[i].key];
+    }
+  }
+
+  await saveToStorage(SAVED_TRANSCRIPTIONS_KEY, updatedTranscriptions)
+}
+
+async function saveTranscription(key, value) {
   const obj = {};
 
   obj[key] = {
@@ -123,6 +217,7 @@ function saveTranscription(key, value) {
       }
     });
   }
+  console.log('-------------------------------------!!!!!!!!! saving transcription>>>', key, obj[key])
+  await saveToStorage(key, obj[key])
 
-  chrome.storage.local.set(obj);
 }
