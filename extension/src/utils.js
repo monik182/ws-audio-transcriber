f82a31cae70e192537ee2876d48fe853a64b5e21
Monica Carvajal <monik.182@gmail.com>
2024-09-20 00:42:17 +0200

    add wait util

--- extension/src/utils.js
+++ extension/src/utils.js
@@ -1,7 +1,7 @@
 export const TOKEN_KEY = 'token';
 
-export const saveToStorage = async (token) => {
-  await chrome.storage.sync.set({ token });
+export const saveToStorage = async (key, value) => {
+  await chrome.storage.sync.set({ [key]: value });
 };
 
 export const removeFromStorage = async (key) => {
@@ -12,3 +12,36 @@ export const getFromStorage = async (key) => {
   const value = await chrome?.storage?.sync?.get?.(key);
   return value[key];
 };
+
+export async function getModels(token) {
+  try {
+    const response = await fetch('https://api.openai.com/v1/models', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }).then(response => response.json())
+    return response;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export function obfuscateToken(token) {
+  const hyphenIndex = token.indexOf('-');
+
+  if (hyphenIndex === -1) {
+    return token;
+  }
+
+  const firstPart = token.slice(0, hyphenIndex + 1);
+  const lastPart = token.slice(-4);
+  return `${firstPart}****${lastPart}`;
+}
+
+export async function wait(ms = 500) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
